da994496159e96bd34a216391ed561da35619f5b
unknown <kinekarpallavi21@gmail.com>
2022-06-13 10:40:45 +0530

    Add:Welcome to snake and Ladder game

---
+++ SnakeAndLadder.php
@@ -0,0 +1,11 @@
+<?php
+/* Program for welcome to 
+    snake and ladder game*/
+class GameOperation{
+    function welcomeGame(){
+        echo"Welcome to Snake and Ladder Game";
+    }
+}
+$obj=new GameOperation();
+$obj->welcomeGame();
+?>
