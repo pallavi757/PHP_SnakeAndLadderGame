d07fd1e5682bfcd5eae9a144a2f35b5bcb872b0e
unknown <kinekarpallavi21@gmail.com>
2022-06-13 22:49:16 +0530

    Add:Using rand to get 0 to 2 for check player will move to which option

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -2,6 +2,10 @@
 /* Program for welcome to  snake and ladder game*/
 class GameOperation
 {
+    const LADDER = 1;
+    const SNAKE = 2;
+    const NOPLAY = 0;
+    public $position = 0;
 
     function welcomeGame()
     {
@@ -16,10 +20,29 @@ class GameOperation
         echo "Player at starting position:" . $startedPosition . "\n";
         echo "Rolling a die ";
         $rollDie = rand(1, 6);
-        $startedPosition+=$rollDie;
-        echo "current position is:".$startedPosition;
-
+       // $startedPosition+=$rollDie;
+       // echo "current position is:".$startedPosition;
+        $this->nextMove($rollDie);
     }
+     //create function to check option for NO Play,Ladder,Snake
+     public function nextMove($rollDie)
+     {
+         $option = rand(0, 2);
+         switch ($option) {
+             case GameOperation::LADDER:
+                 echo " player on Ladder move to next position " . $this->position += $rollDie;
+                 echo "\n";
+                 break;
+             case GameOperation::SNAKE:
+                 echo " player on snake move to previous Position " . $this->position -= $rollDie;
+                 echo "\n";
+                 break;
+             default:
+                 echo " No Play " . $this->position = $this->position;
+                 echo "\n";
+         }
+         echo "Current Position is:" . $this->position . "\n";
+     }
 }
 $obj = new GameOperation();
 $obj->welcomeGame();
