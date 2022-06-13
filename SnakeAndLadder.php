498dc736c2b9f90ef08ee904731177876fce22f7
unknown <kinekarpallavi21@gmail.com>
2022-06-13 10:47:51 +0530

    Add:Set initial postion to 0 for single player

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -2,6 +2,8 @@
 /* Program for welcome to 
     snake and ladder game*/
 class GameOperation{
+    //set start postion at 0
+    private $startPosition = 0;
     function welcomeGame(){
         echo"Welcome to Snake and Ladder Game";
     }
