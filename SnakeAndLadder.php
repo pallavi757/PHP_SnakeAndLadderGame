03895bc4f660db16fa318cfe2ecca12fcc8a919e
unknown <kinekarpallavi21@gmail.com>
2022-06-13 22:37:41 +0530

    Add:Using rand to get 1 to 6 on die

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -1,13 +1,27 @@
 <?php
-/* Program for welcome to 
-    snake and ladder game*/
-class GameOperation{
-    //set start postion at 0
-    private $startPosition = 0;
-    function welcomeGame(){
-        echo"Welcome to Snake and Ladder Game";
+/* Program for welcome to  snake and ladder game*/
+class GameOperation
+{
+
+    function welcomeGame()
+    {
+        echo "Welcome to Snake and Ladder Game \n";
+    }
+
+    /*function for palyer roll a die and
+     get number between 1 to 6*/
+    public function diceRoll()
+    {
+        $startedPosition = 0;
+        echo "Player at starting position:" . $startedPosition . "\n";
+        echo "Rolling a die ";
+        $rollDie = rand(1, 6);
+        $startedPosition+=$rollDie;
+        echo "current position is:".$startedPosition;
+
     }
 }
-$obj=new GameOperation();
+$obj = new GameOperation();
 $obj->welcomeGame();
+$obj->diceRoll();
 ?>
