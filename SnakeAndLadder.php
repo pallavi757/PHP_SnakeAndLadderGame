253915e19304bccb5bb0cc899884958aac5f12a0
unknown <kinekarpallavi21@gmail.com>
2022-06-14 23:21:54 +0530

    Add:Modified UC4 repeating process till player reaches 100

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -12,29 +12,22 @@ class GameOperation
         echo "Welcome to Snake and Ladder Game \n";
     }
 
-    /*function for palyer roll a die and
-     get number between 1 to 6*/
-    public function diceRoll()
-    {
-        $startedPosition = 0;
-        echo "Player at starting position:" . $startedPosition . "\n";
-        echo "Rolling a die ";
-        $rollDie = rand(1, 6);
-       // $startedPosition+=$rollDie;
-       // echo "current position is:".$startedPosition;
-        $this->nextMove($rollDie);
-    }
+   
      //create function to check option for NO Play,Ladder,Snake
-     public function nextMove($rollDie)
-     {
-         $option = rand(0, 2);
+     public function nextMove(){    
+        while($this->position!=100) {
+        $rollDie = $this->diceRoll();
+         $option = rand(0, 2); // options to play
+       // echo "Option:".$option ."\n";
          switch ($option) {
              case GameOperation::LADDER:
-                 echo " player on Ladder move to next position " . $this->position += $rollDie;
+                echo " player on Ladder move to next position";
+                   $this->position += $rollDie;
                  echo "\n";
                  break;
              case GameOperation::SNAKE:
-                 echo " player on snake move to previous Position " . $this->position -= $rollDie;
+                echo " player on snake move to previous Position";
+                $this->position -= $rollDie;
                  echo "\n";
                  break;
              default:
@@ -42,9 +35,26 @@ class GameOperation
                  echo "\n";
          }
          echo "Current Position is:" . $this->position . "\n";
+        }
      }
+
+      /*function for palyer roll a die and
+     get number between 1 to 6*/
+    function diceRoll()
+    {
+       // $startedPosition = 0;
+        //echo "Player at starting position:" . $startedPosition . "\n";
+        echo "Rolling a die ";
+        $rollDie = rand(1, 6);
+       // $startedPosition+=$rollDie;
+        echo "getting a die number is:".$rollDie;
+        return  $rollDie;
+      // $this->nextMove();
+    }
 }
 $obj = new GameOperation();
 $obj->welcomeGame();
-$obj->diceRoll();
+//$obj->diceRoll();
+$obj->nextMove();
+echo "Player won";
 ?>
